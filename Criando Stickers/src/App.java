8734e2776f7ceba797db0f7cc16ce831dcfe2d8f
David Alves <david.alvess633@gmail.com>
2022-07-23 09:37:12 -0300

    Atualizando aula

---
+++ Criando Stickers/src/App.java
@@ -0,0 +1,46 @@
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+public class App {
+    private static Object InputStream;
+
+
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+
+        var key = "k_fjtlk5gc";
+        String url = "https://imdb-api.com/en/API/Top250Movies/" + key;
+        ExtratoDeConteudo extrator = new ExtratorDoConteudoIMDB();
+
+
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+        // exibir e manipular os dados
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+        
+
+        var geradora = new GeradoraDeFigurinhas();
+
+        for (int i = 0; i < 3; i++) {
+
+            Conteudo conteudo = conteudos.get(i);
+
+        
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+ 
+
+            System.out.println(conteudo.getTitulo());
+            System.out.println();
+
+        }
+    }
+}
